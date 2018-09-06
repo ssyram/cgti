e8a0b4da65a5af8627c13fb311a9ce29b0e15660
ssyram <liguany126@126.com>
2018-09-06 14:47:20 +0800

    new project.

---
+++ ChomskyGrammerTypeIdentifier/ChomskyGrammerTypeIdentifier/ChomskyGrammer.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CHOMSKY_GRAMMER
+#define CHOMSKY_GRAMMER
+
+//任务全过程：读取输入 -> 分析输入，提取符号 -> 构成文法对象 -> 采用getType()获得具体类别信息
+/*
+ * 判别器目标使用：
+ * ChomskeyGrammer grammer();
+ * grammer.setU
+ */
+
+#endif // !CHOMSKY_GRAMMER_TYPE_IDENTIFIER
